818fb49e8510e54ce22a931df7ec9237712d64ea
wasteproduct <27861665+wasteproduct@users.noreply.github.com>
2021-02-09 17:22:26 +0900

    210209_1

---
+++ Assets/System/UIBase/NotchResolutionMargin.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotchResolutionMargin : MonoBehaviour
+{
+    [SerializeField] private Image top;
+    [SerializeField] private Image left;
+    [SerializeField] private Image right;
+    [SerializeField] private Image bottom;
+
+    public void SetMargins(RectTransform rectTransform)
+    {
+        Canvas canvas = GetComponentInParent<Canvas>();
+        if (canvas == null) return;
+
+        RectTransform canvasTransform = GetComponent<RectTransform>();
+        if (canvasTransform == null) return;
+
+        RectTransform resolutionTransform = GetComponent<RectTransform>();
+
+        Vector3[] marginCorners = new Vector3[4];
+        resolutionTransform.GetWorldCorners(marginCorners);
+
+        Vector3[] panelCorners = new Vector3[4];
+        rectTransform.GetWorldCorners(panelCorners);
+
+        float widthRatio = canvasTransform.lossyScale.x;
+        float heightRatio = canvasTransform.lossyScale.y;
+
+        top.rectTransform.sizeDelta = new Vector2(0, (marginCorners[1].y - panelCorners[1].y) / heightRatio);
+        left.rectTransform.sizeDelta = new Vector2((panelCorners[0].x - marginCorners[0].x) / widthRatio, 0);
+        right.rectTransform.sizeDelta = new Vector2((marginCorners[2].x - panelCorners[2].x) / widthRatio, 0);
+        bottom.rectTransform.sizeDelta = new Vector2(0, (panelCorners[0].y - marginCorners[0].y) / heightRatio);
+    }
+}
---
+++ Assets/System/UIBase/NotchResolutionResolver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotchResolutionResolver : MonoBehaviour
+{
+    [SerializeField] private NotchResolutionMargin margin;
+
+    void Awake()
+    {
+        RectTransform rectTransform = GetComponent<RectTransform>();
+
+        Vector2 anchorMin = Screen.safeArea.position;
+        Vector2 anchorMax = Screen.safeArea.position + Screen.safeArea.size;
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+        rectTransform.anchorMin = anchorMin;
+        rectTransform.anchorMax = anchorMax;
+
+        if (margin != null) margin.SetMargins(rectTransform);
+    }
+}
