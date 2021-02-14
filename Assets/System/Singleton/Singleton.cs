fe8ee56ea07e3106d07f8b87937fda6c06d4153d
Waywaydev <wayway.dev@gmail.com>
2021-02-15 00:12:16 +0900

    SingletonPackage Import

---
+++ Assets/System/Singleton/Singleton.cs
@@ -0,0 +1,31 @@
+﻿/* SCRIPT WRITTEN BY
+ * Singleton.cs
+ * Wayway Studio
+ * Singleton
+ * inherit Class
+ * nothing require
+ * 2021.02.15 */
+public class Singleton<T> where T : class, new()
+{
+    private static T instance;
+
+    private static object _lock = new object();
+
+    public static T Instance
+    {
+        get
+        {
+            lock (_lock)
+            {
+                if (instance == null)
+                {
+                    instance = new T();
+                }
+
+                return instance;
+            }
+        }
+    }
+
+    protected Singleton() { }
+}
