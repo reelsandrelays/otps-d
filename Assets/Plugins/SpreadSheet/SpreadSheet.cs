b8ba7ef1c8cd93912d061f2271232ef385cc5d68
Waywaydev <wayway.dev@gmail.com>
2021-02-16 20:11:49 +0900

    SpreadSheet & Nuget Import

---
+++ Assets/Plugins/SpreadSheet/SpreadSheet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[HideMonoScript]
+public class SpreadSheet : ScriptableObject
+{
+    #region FIELD
+    [DisplayAsString] public string Title;
+    [DisplayAsString] public string ID;
+    [DisplayAsString] public string Url;
+    [DisplayAsString] public int Headline = 2;
+
+    [ListDrawerSettings(HideRemoveButton = true, HideAddButton = true, IsReadOnly = true, Expanded = true), PropertySpace(SpaceBefore = 10f)]
+    public List<WorkSheet> WorkSheetList;
+    #endregion    
+
+    public WorkSheet GetSheet(string worksheetName)
+    {
+        foreach(var workSheet in WorkSheetList)
+        {
+            if (workSheet.Title.Equals(worksheetName))
+                return workSheet;
+        }
+
+        Debug.LogError("Can't Find <color=red>" + worksheetName + " </color>Sheet");
+        return null;
+    }
+}
